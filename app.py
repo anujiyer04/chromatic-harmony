90af8e0c84521b29eeef14e8fbb0ed9b21881cc5
anujiyer04 <anujiyer204@gmail.com>
2025-03-30 00:13:59 +0530

    fixed

--- app.py
+++ app.py
@@ -16,8 +16,6 @@ from dotenv import load_dotenv
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
 
-load_dotenv()
-
 SPOTIPY_CLIENT_ID = os.getenv("SPOTIPY_CLIENT_ID")
 SPOTIPY_CLIENT_SECRET = os.getenv("SPOTIPY_CLIENT_SECRET")
 SPOTIPY_REDIRECT_URI = os.getenv("SPOTIPY_REDIRECT_URI")
